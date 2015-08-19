624cf61da270e1640cec7b303c662ab0f1b13cd2
chuckleone <chuckleone@gmail.com>
2015-08-19 15:43:42 -0400

    Initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,188 @@
+angular.module('starter.controllers', [])
+
+.controller('RobotCtrl', function($scope, Robots) {
+   $scope.robots = Robots.all();	
+})
+
+.controller('FactoryCtrl', function($scope, Robots) {
+    
+	$scope.state = {
+		factoryEmpty: true
+	};
+	
+	// star field
+	/* Randomly generated star field by Chuck Leone
+	/* Use as you like, please credit me if you do
+	/* email: chuckleone@gmail.com
+	/* twitter @ChuckLeone 
+	*/ 
+	
+	var canvas = document.getElementById('main'),
+	context = canvas.getContext('2d');
+	
+	var myStars = function(){
+
+		for (i=1; i<=60;  i++) {	
+			var rndX = Math.floor((Math.random()*640) +1);
+			var rndY = Math.floor((Math.random()*480) +1);
+			x = rndX;
+			y = rndY;
+			context.fillStyle = 'rgb(255, 255, 255)'; // white
+			context.fillRect(x, y, 1, 1);
+		};
+		
+		for (i=1; i<=40;  i++) {	
+			var rndX = Math.floor((Math.random()*640) +1);
+			var rndY = Math.floor((Math.random()*480) +1);
+			x = rndX;
+			y = rndY;
+			context.fillStyle = 'rgb(80, 80, 80)'; // grey
+			context.fillRect(x, y, 1, 1);
+		};
+		
+		for (i=1; i<=10;  i++) {	
+			var rndX = Math.floor((Math.random()*640) +1);
+			var rndY = Math.floor((Math.random()*480) +1);
+			x = rndX;
+			y = rndY;
+			context.fillStyle = 'rgb(255, 100, 100)'; // red
+			context.fillRect(x, y, 1, 1);
+		};
+		
+		for (i=1; i<=10;  i++) {	
+			var rndX = Math.floor((Math.random()*640) +1);
+			var rndY = Math.floor((Math.random()*480) +1);
+			x = rndX;
+			y = rndY;
+			context.fillStyle = 'rgb(86, 171, 255)'; // blue
+			context.fillRect(x, y, 1, 1);
+		};
+		
+		for (i=1; i<=10;  i++) {	
+			var rndX = Math.floor((Math.random()*640) +1);
+			var rndY = Math.floor((Math.random()*480) +1);
+			x = rndX;
+			y = rndY;
+			context.fillStyle = 'green'; // green
+			context.fillRect(x, y, 2, 2);
+		};
+		
+		for (i=1; i<=5;  i++) {	
+			var rndX = Math.floor((Math.random()*640) +1);
+			var rndY = Math.floor((Math.random()*480) +1);
+			x = rndX;
+			y = rndY;
+			context.fillStyle = 'rgb(255, 255, 255)'; //white
+			context.fillRect(x, y, 2, 2);
+		}
+		// build a planet
+		for (i=1; i<=1;  i++) {	
+			var rndX = Math.floor((Math.random()*640) +1);
+			var rndY = Math.floor((Math.random()*480) +1);
+			x = rndX;
+			y = rndY;
+			context.fillStyle = 'rgb(155, 255, 251)'; // mint green
+			context.fillRect(x, y, 4, 4);
+			context.fillRect(x+1, y-1, 1, 1);
+			context.fillRect(x+2, y-1, 1, 1);
+			context.fillRect(x+4, y+2, 1, 1);
+			context.fillRect(x-1, y+1, 1, 1);
+			context.fillRect(x-1, y+2, 1, 1);
+			context.fillRect(x+1, y+4, 1, 1);
+			context.fillRect(x+2, y+4, 1, 1);
+			context.fillRect(x+4, y+1, 1, 1);
+			context.fillRect(x+4, y+2, 1, 1);
+		}
+		return;
+	};
+	
+	myStars();
+  // end star field
+  
+	$scope.createRobot = function() {
+		$scope.robot = '';
+		$scope.robotName = '';
+		var canvas = document.getElementById('main'),
+		context = canvas.getContext('2d');
+		
+		var x = 150;
+		var y = 100;
+		
+		context.fillStyle= randomColor();
+		
+		// enemy body
+		context.fillRect(x, y, 20, 20);
+		
+		// enemy arms
+		context.fillRect(x+20, y+2, 6, 4);
+		context.fillRect(x-6, y+2, 6, 4);
+		context.fillRect(x+22, y+4, 4, 10);
+		context.fillRect(x-6, y+4, 4, 10);
+	
+		// enemy legs
+		context.fillRect(x+2, y+25, 4, 15);
+		context.fillRect(x+15, y+25, 4, 15);
+		context.fillRect(x+2, y+2, 14, 4)
+	
+		// enemy eye - use for easier modes
+		context.clearRect(x+2, y+2, 14, 4);
+		context.fillStyle = 'rgb(255, 0, 0)';
+		context.fillRect(x+6, y+2, 6, 4);
+		
+
+		function randomColor() {
+					for (var n = 0; n < 1; n ++) {
+						var letters = '0123456789ABCDEF'.split('');
+						var color = '#';
+							for (var i = 0; i < 6; i++ ) {
+								color += letters[Math.round(Math.random() * 15)];
+							}
+				} 
+			
+			$scope.robotName = color;
+			
+			return color;
+		}
+		$scope.state.factoryEmpty = false;
+		console.log("robot name is " + $scope.robotName);
+		return $scope.robotName;
+		return $scope.robot = [{
+			name: $scope.robotName,
+			id: $scope.robotId
+		}]
+	};
+
+	$scope.destroyRobot = function() {
+		$scope.state.factoryEmpty = true;
+	}
+	
+	$scope.saveRobot = function() {
+		console.log($scope.robot);
+		Robots.add($scope.robot);
+	}
+})
+
+.controller('WarehouseCtrl', function($scope, Robots) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.robots = Robots.all();
+  $scope.remove = function(robot) {
+    Robots.remove(robot);
+  };
+})
+
+.controller('RobotDetailCtrl', function($scope, $stateParams, Robots) {
+  $scope.robot = Robots.get($stateParams.robotId);
+})
+
+.controller('LabCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
---
+++ www/js/services.js
@@ -0,0 +1,79 @@
+angular.module('starter.services', [])
+
+.factory('Robots', function(){
+  var robots = [{
+    id: 0,
+    name: 'R2D2',
+  },{
+    id: 1,
+    name: 'THX1138'
+  }];
+  return {
+    all: function() {
+      return robots;
+    },
+    remove: function(robots) {
+      robots.splice(robots.indexOf(robots), 1);
+    },
+    add: function(robots) {
+      robots.splice(robots.indexOf(robots), 0)
+    },
+    get: function(robotId) {
+      for (var i = 0; i < robots.length; i++) {
+        if (robots[i].id === parseInt(robotId)) {
+          return robots[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
